6201b6a006b2120b31858cd4791e51864e67a970
ewerton santos <ewertonsantos.job7@gmail.com>
2025-12-04 18:46:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9840306402-6c969",
+  "appId": "1:726386228816:web:3c9a796e033c7b2d5a1b55",
+  "apiKey": "AIzaSyB5Kj4Ae7FuzoGxrqORpE50v59O2uELyHg",
+  "authDomain": "studio-9840306402-6c969.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "726386228816"
+};
