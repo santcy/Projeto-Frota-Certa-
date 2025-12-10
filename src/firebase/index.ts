2d76d718a9d6963a0a0d8963042db22f0756d2e7
ewerton santos <ewertonsantos.job7@gmail.com>
2025-12-10 18:34:32 +0000

    Try fixing this error: `Runtime FirebaseError: "projectId" not provided

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,12 +3,19 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { Auth, getAuth } from 'firebase/auth';
-import { Firestore, getFirestore, clearIndexedDbPersistence, enableIndexedDbPersistence, initializeFirestore, memoryLocalCache } from 'firebase/firestore';
+import {
+  Firestore,
+  getFirestore,
+  clearIndexedDbPersistence,
+  enableIndexedDbPersistence,
+  initializeFirestore,
+  memoryLocalCache,
+} from 'firebase/firestore';
 
 function getSdks(firebaseApp: FirebaseApp) {
   const firestore = getFirestore(firebaseApp);
   const auth = getAuth(firebaseApp);
-  
+
   return {
     firebaseApp,
     auth,
@@ -22,32 +29,33 @@ export function initializeFirebase() {
     return getSdks(getApp());
   }
 
-  let app;
-  try {
-    // This will throw if the hosting environment is not detected.
-    app = initializeApp({});
-  } catch (e) {
-    app = initializeApp(firebaseConfig);
-  }
+  // Always initialize with the config. In a production Firebase Hosting
+  // environment, the SDK will automatically ignore this and use the
+  // reserved endpoint, but in a local environment this is required.
+  const app = initializeApp(firebaseConfig);
 
   // Initialize Firestore with memory cache for both server and client.
   initializeFirestore(app, {
-      localCache: memoryLocalCache(),
+    localCache: memoryLocalCache(),
   });
-  
+
   // This part should only run on the client to enable IndexedDB persistence
   if (typeof window !== 'undefined') {
     const firestoreInstance = getFirestore(app);
     clearIndexedDbPersistence(firestoreInstance)
       .then(() => enableIndexedDbPersistence(firestoreInstance))
       .catch((err) => {
-          if (err.code == 'failed-precondition') {
-              console.warn('Firestore persistence failed to enable. This can happen if you have multiple tabs open.');
-          } else if (err.code == 'unimplemented') {
-              console.warn('The current browser does not support all of the features required to enable Firestore persistence.');
-          } else {
-              console.error("Failed to initialize Firestore persistence:", err);
-          }
+        if (err.code == 'failed-precondition') {
+          console.warn(
+            'Firestore persistence failed to enable. This can happen if you have multiple tabs open.'
+          );
+        } else if (err.code == 'unimplemented') {
+          console.warn(
+            'The current browser does not support all of the features required to enable Firestore persistence.'
+          );
+        } else {
+          console.error('Failed to initialize Firestore persistence:', err);
+        }
       });
   }
 
