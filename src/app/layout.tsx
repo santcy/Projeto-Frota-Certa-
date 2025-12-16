ca956abccc652175b61ccb27132377b9ca5ad709
ewerton santos <ewertonsantos.job7@gmail.com>
2025-12-16 14:25:40 +0000

    ajuste para preenchar toda a tela

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
                   </Sidebar>
                   <div className="flex flex-1 flex-col">
                     <Header />
-                    <main className="flex-1 overflow-y-auto bg-background p-4 md:p-6 lg:p-8">
+                    <main className="flex-1 overflow-y-auto bg-background">
                       {children}
                     </main>
                   </div>
